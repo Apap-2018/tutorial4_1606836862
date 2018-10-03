941d19be6f6f61d81abafab1a1a60b93d5133acd
kemalaAndiyani <kemala.a@gmail.com>
2018-10-03 17:48:31 +0700

    initial commit lab 4

---
+++ src/main/java/com/apap/tutorial4/service/PilotServiceImpl.java
@@ -0,0 +1,25 @@
+package com.apap.tutorial4.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.apap.tutorial4.model.PilotModel;
+import com.apap.tutorial4.reporsitory.PilotDB;
+
+@Service
+@Transactional
+public class PilotServiceImpl implements PilotService {
+	@Autowired
+	private PilotDB pilotDb;
+	
+	@Override
+	public PilotModel getPilotDetailByLicenseNumber(String licenseNumber) {
+		return pilotDb.findByLicenseNumber(licenseNumber);
+	}
+	
+	@Override
+	public void addPilot(PilotModel pilot) {
+		pilotDb.save(pilot);
+	}
+}
